9d848a5f09f8d3bd123b4d62ea8bdc82f652ae16
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-01-12 09:49:28 +0200

    Storage class refactored

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -1,37 +1,17 @@
 package core.basesyntax.db;
 
-import core.basesyntax.model.Car;
-import core.basesyntax.model.Driver;
 import core.basesyntax.model.Manufacturer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
-    public static final List<Driver> drivers = new ArrayList<>();
     public static final List<Manufacturer> manufacturers = new ArrayList<>();
-    public static final List<Car> cars = new ArrayList<>();
-    private static long driverId = 0;
     private static long manufacturerId = 0;
-    private static long carId = 0;
-
-    public static Driver addDriver(Driver driver) {
-        driverId++;
-        driver.setId(driverId);
-        drivers.add(driver);
-        return driver;
-    }
 
     public static Manufacturer addManufacturer(Manufacturer manufacturer) {
         manufacturerId++;
         manufacturer.setId(manufacturerId);
         manufacturers.add(manufacturer);
         return manufacturer;
-    }
-
-    public static Car addCar(Car car) {
-        carId++;
-        car.setId(carId);
-        cars.add(car);
-        return car;
     }
 }
