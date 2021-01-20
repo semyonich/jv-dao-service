5c14ec81460557143f74f760a92d79a4e7194a47
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-01-20 12:30:35 +0200

    Revert "added minor fixes to CarDaoJdbcImpl class"
    This reverts commit 07e8c641

--- src/main/java/core/basesyntax/dao/CarDao.java
+++ src/main/java/core/basesyntax/dao/CarDao.java
@@ -2,7 +2,18 @@ package core.basesyntax.dao;
 
 import core.basesyntax.model.Car;
 import java.util.List;
+import java.util.Optional;
 
-public interface CarDao extends GenericDao<Car, Long> {
+public interface CarDao {
+    Car create(Car car);
+
+    Optional<Car> get(Long id);
+
+    List<Car> getAll();
+
+    Car update(Car car);
+
+    boolean delete(Long id);
+
     List<Car> getAllByDriver(Long driverId);
 }
